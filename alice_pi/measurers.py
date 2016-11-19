a71e1470d9d7f1d1ecddf6c7912a0eed587089e1
jlmcgehee21 <jlmcgehee21@gmail.com>
2016-11-18 23:38:28 -0500

    Tested temperature measurer

--- alice_pi/measurers.py
+++ alice_pi/measurers.py
@@ -1,13 +1,22 @@
+"""
+A Measurer's responsibility is to interact with the sensor
+hardware and take a measurement, then return that measurement
+"""
+from w1thermsensor import W1ThermSensor
 
-class Measurer(object):
-    """
-    A Measurer's responsibility is to interact with the sensor
-    hardware and take a measurement, then return that measurement
-    """
-    pass
+class TemperatureMeasurer(object):
+    unit_dict = {'F': W1ThermSensor.DEGREES_F,
+                 'C': W1ThermSensor.DEGREES_C}
 
-class TemperatureMeasurer(Measurer):
-    pass
+    def __init__(self, unit='F', n_decimals=2):
+        self.unit = self.unit_dict[unit]
+        self.n_decimals = n_decimals
 
-class MotionMeasurer(Measurer):
+    def measure(self):
+        sensor = W1ThermSensor()
+        temp = sensor.get_temperature(self.unit)
+
+        return round(temp, self.n_decimals)
+
+class MotionMeasurer(object):
     pass
---
+++ alice_pi/tests/test_measurers.py
@@ -0,0 +1,26 @@
+import unittest2 as unittest
+import mock
+from alice_pi import measurers
+import random
+from w1thermsensor import W1ThermSensor
+
+class TestTemperatureMeasurer(unittest.TestCase):
+
+    @mock.patch('alice_pi.measurers.W1ThermSensor')
+    def test_measure(self, mock_get_temp):
+        test_temp = 10 + random.random()
+
+        mock_get_temp = mock_get_temp.return_value
+        mock_get_temp.get_temperature = mock.MagicMock()
+        mock_get_temp.get_temperature.return_value = test_temp
+
+        meas = measurers.TemperatureMeasurer('F', n_decimals=2)
+
+        temp = meas.measure()
+
+        self.assertEquals(temp, round(test_temp, 2))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
