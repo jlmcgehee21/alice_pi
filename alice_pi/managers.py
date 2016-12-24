702f6c1a1c20e2950d2de5e3199d6f7c66e39516
jlmcgehee21 <jlmcgehee21@gmail.com>
2016-12-23 22:07:57 -0500

    manager tested

--- alice_pi/managers.py
+++ alice_pi/managers.py
@@ -1,3 +1,5 @@
+import asyncio
+
 
 class Manager(object):
     """
@@ -7,8 +9,47 @@ class Manager(object):
     def __init__(self, measurers_and_senders):
         self.measurers_and_senders = measurers_and_senders
 
-    def run(self):
+    def __init_measurements(self):
+        for measurer_group in self.measurers_and_senders:
+            measurer_group['measurement'] = None
+
+    def run(self, count=None):
         """
         For every measurer, see if the value has changed, if it has,
         push to senders.
         """
+
+        async def check_status(measurer_group):
+            new_status = measurer_group['measurer'].measure()
+
+            if new_status != measurer_group['measurement']:
+                return new_status
+            else:
+                return None
+
+        async def send_measurement(sender, measurement):
+            sender.send(measurement)
+
+        async def my_coroutine():
+            loop_count = 0
+            self.__init_measurements()
+            while True:
+                for measurer_group in self.measurers_and_senders:  # async for
+                    meas_status = await check_status(measurer_group)
+                    if meas_status is not None:
+                        measurer_group['measurement'] = meas_status
+                        for sender in measurer_group['senders']:  # async for
+                            await send_measurement(sender,
+                                                   measurer_group['measurement'])
+
+                if count is not None:
+                    loop_count += 1
+                    if loop_count >= count:
+                        break
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(my_coroutine())
+
+
+if __name__ == '__main__':
+    Manager('').run()
--- alice_pi/tests/test_managers.py
+++ alice_pi/tests/test_managers.py
@@ -1,9 +1,28 @@
 import unittest2 as unittest
+import mock
+from alice_pi import managers
+
 
 class TestManager(unittest.TestCase):
-    def test_(self):
-        pass
+    def test_run(self):
+        count = 3
+
+        measurer = mock.MagicMock(measure=mock.Mock(side_effect=lambda:  1))
+        sender = mock.MagicMock(send=mock.Mock(side_effect=lambda x: 200))
+
+        test = {'measurer': measurer,
+                'senders': [sender]}
+
+        man = managers.Manager([test])
+
+        man.run(count=count)
+
+        # assert we check the measurer every time
+        self.assertEquals(measurer.measure.call_count, count)
+
+        # since we are recording the same measurement, we will only send once
+        self.assertEquals(sender.send.call_count, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
-
